7694cc3267ffd5a176cd7f0fdd2a812d70f8bf48
Hussain4811 <hussaingoraya981@gmail.com>
2024-07-15 17:02:53 +0500

    ...

--- src/Components/Client.jsx
+++ src/Components/Client.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './Nav.css'
+import Navbar from "./Navbar";
 
 export default function Client() {
   return (
     <>
 
+<Navbar/>
 
     <div className="container">
 
