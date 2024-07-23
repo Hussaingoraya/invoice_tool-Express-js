da069c929396df3dbd991ea9ba7f3bce1ccbd354
Hussain4811 <hussaingoraya981@gmail.com>
2024-07-23 10:19:50 +0500

    UI ADD INVOICE

--- src/Components/Invoice/AddInovice.jsx
+++ src/Components/Invoice/AddInovice.jsx
@@ -1,38 +1,79 @@
 import React, { useState, useEffect } from "react";
+import Navbar from "../Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 export default function AddInovice() {
   const [details, setDetails] = useState(false);
-  const [tableData, setTableData] = useState(false);
-  const [description, setDescription] = useState("");
-  const [additionalDetails, setAdditionalDetails] = useState("");
-  const [rate, setRate] = useState("");
-  const [quantity, setQuantity] = useState("");
-  const [tax, setTax] = useState("");
-  const [withouttax, setwithoutTax] = useState("");
-  const [totalAmount, setTotalAmount] = useState(0);
-  const hidetableData = (e) => {
-    e.preventDefault();
-    setTableData(!tableData);
-  };
+  const [subtotal, setSubtotal] = useState(0);
+  const [total, setTotal] = useState(0);
+
   const hideDetails = (e) => {
     e.preventDefault();
     setDetails(!details);
   };
-  const calculateTotalAmount = () => {
-    const rateValue = parseFloat(rate) || 0;
-    const quantityValue = parseInt(quantity) || 0;
-    const taxValue = parseFloat(tax) || 0;
+
+  const [rows, setRows] = useState([
+    {
+      description: "",
+      additionalDetails: "",
+      rate: 0,
+      quantity: 0,
+      tax: 0,
+      totalAmount: 0,
+    },
+  ]);
+
+  const handleInputChange = (index, event) => {
+    const { name, value } = event.target;
+    const newRows = [...rows];
+    newRows[index][name] = value;
+    const rateValue = parseFloat(newRows[index].rate) || 0;
+    const quantityValue = parseInt(newRows[index].quantity) || 0;
+    const taxValue = parseFloat(newRows[index].tax) || 0;
     const amount = rateValue * quantityValue;
     const total = amount + (amount * taxValue) / 100;
-    setwithoutTax(amount)
-    setTotalAmount(total);
+
+    newRows[index].totalAmount = total;
+
+    setRows(newRows);
+  };
+
+  const addRow = () => {
+    setRows([
+      ...rows,
+      {
+        description: "",
+        additionalDetails: "",
+        rate: 0,
+        quantity: 0,
+        tax: 0,
+        totalAmount: 0,
+      },
+    ]);
+  };
+
+  const removeRow = (index) => {
+    const newRows = [...rows];
+    newRows.splice(index, 1);
+    setRows(newRows);
   };
 
   useEffect(() => {
-    calculateTotalAmount();
-  }, [rate, quantity, tax]);
+    const subtotalAmount = rows.reduce(
+      (acc, row) =>
+        acc + (parseFloat(row.rate) || 0) * (parseInt(row.quantity) || 0),
+      0
+    );
+    const totalAmount = rows.reduce((acc, row) => acc + row.totalAmount, 0);
+
+    setSubtotal(subtotalAmount);
+    setTotal(totalAmount);
+  }, [rows]);
+
   return (
     <>
+      <Navbar />
       <div className="container main-invoice">
         <div className="bg-grey-100">
           <div className="row">
@@ -191,8 +232,44 @@ export default function AddInovice() {
                         </div>
                       </div>
                     )}
+
+                    <div className="row g-3 align-items-center ">
+                      <div className="col-md-2">
+                        <label
+                          htmlFor="invoice-number"
+                          className="col-form-label inputText "
+                        >
+                          Number
+                        </label>
+                      </div>
+                      <div className="col-md-10">
+                        <input
+                          type="tel"
+                          id="invoice-number"
+                          className="form-control"
+                        />
+                      </div>
+                    </div>
+                    <div className="row g-3 align-items-center ">
+                      <div className="col-md-2">
+                        <label
+                          htmlFor="invoice-date"
+                          className="col-form-label inputText my-3"
+                        >
+                          Date
+                        </label>
+                      </div>
+                      <div className="col-md-10">
+                        <input
+                          type="date"
+                          id="invoice-date"
+                          className="form-control"
+                        />
+                      </div>
+                    </div>
                   </form>
                 </div>
+                {/* <hr /> */}
                 <div className="col-6">
                   <div>
                     <h5>Bill To</h5>
@@ -232,7 +309,7 @@ export default function AddInovice() {
                         />
                       </div>
                     </div>
-                    <div className="row g-3  ">
+                    <div className="row g-3 align-items-center  ">
                       <div className="col-md-2">
                         <label
                           htmlFor="userAddress1"
@@ -247,18 +324,6 @@ export default function AddInovice() {
                           id="userAddress1"
                           className="form-control my-2 "
                           placeholder="Street"
-                        />
-                        <input
-                          type="text"
-                          id="userAddress2"
-                          className="form-control my-2"
-                          placeholder="City"
-                        />
-                        <input
-                          type="text"
-                          id="userAddress3"
-                          className="form-control my-2"
-                          placeholder="State"
                         />
                       </div>
                     </div>
@@ -314,89 +379,101 @@ export default function AddInovice() {
                   </tr>
                 </thead>
                 <tbody>
+                  {rows.map((row, index) => (
+                    <tr key={index}>
+                      <td>
+                        <button
+                          className="addRemove"
+                          onClick={() => removeRow(index)}
+                        >
+                          <FontAwesomeIcon icon={faMinus} />
+                        </button>
+                      </td>
+                      <td>
+                        <div className="col-md-10">
+                          <input
+                            type="text"
+                            id={`itemDescription-${index}`}
+                            name="description"
+                            className="form-control"
+                            placeholder="Item Description"
+                            value={row.description}
+                            onChange={(e) => handleInputChange(index, e)}
+                          />
+                        </div>
+                        <div>
+                          <textarea
+                            id={`additionalDetails-${index}`}
+                            name="additionalDetails"
+                            cols="30"
+                            rows="3"
+                            placeholder="Additional Details"
+                            className="mt-3 form-control"
+                            value={row.additionalDetails}
+                            onChange={(e) => handleInputChange(index, e)}
+                          ></textarea>
+                        </div>
+                      </td>
+                      <td>
+                        <div className="price">
+                          <input
+                            type="number"
+                            id={`rate-${index}`}
+                            name="rate"
+                            className="form-control"
+                            placeholder="Rate"
+                            value={row.rate}
+                            onChange={(e) => handleInputChange(index, e)}
+                          />
+                        </div>
+                      </td>
+                      <td>
+                        <div className="qty">
+                          <input
+                            type="number"
+                            id={`quantity-${index}`}
+                            name="quantity"
+                            className="form-control"
+                            placeholder="Quantity"
+                            value={row.quantity}
+                            onChange={(e) => handleInputChange(index, e)}
+                          />
+                        </div>
+                      </td>
+                      <td>
+                        <div className="tax">
+                          <input
+                            type="number"
+                            id={`tax-${index}`}
+                            name="tax"
+                            className="form-control"
+                            placeholder="Tax"
+                            value={row.tax}
+                            onChange={(e) => handleInputChange(index, e)}
+                          />
+                        </div>
+                      </td>
+                      <td>
+                        <div className="amount">
+                          {row.totalAmount.toFixed(2)}
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
                   <tr>
-                    <td>
-                      <button className="addRemove" onClick={hidetableData}>
-                        {tableData ? "+" : "-"}
+                    <td colSpan="6">
+                      <button className="addRemove" onClick={addRow}>
+                        <FontAwesomeIcon icon={faPlus} />
                       </button>
                     </td>
-                    {tableData && (
-                      <>
-                        <td>
-                          <div className="col-md-10">
-                            <input
-                              type="text"
-                              id="itemDescription"
-                              className="form-control"
-                              placeholder="Item Description"
-                              value={description}
-                              onChange={(e) => setDescription(e.target.value)}
-                            />
-                          </div>
-                          <div>
-                            <textarea
-                              id="additionalDetails"
-                              cols="30"
-                              rows="3"
-                              placeholder="Additional Details"
-                              className="mt-3 form-control"
-                              value={additionalDetails}
-                              onChange={(e) =>
-                                setAdditionalDetails(e.target.value)
-                              }
-                            ></textarea>
-                          </div>
-                        </td>
-                        <td>
-                          <div className="price">
-                            <input
-                              type="number"
-                              id="rate"
-                              className="form-control"
-                              placeholder="Rate"
-                              value={rate}
-                              onChange={(e) => setRate(e.target.value)}
-                            />
-                          </div>
-                        </td>
-                        <td>
-                          <div className="qty">
-                            <input
-                              type="number"
-                              id="quantity"
-                              className="form-control"
-                              placeholder="Quantity"
-                              value={quantity}
-                              onChange={(e) => setQuantity(e.target.value)}
-                            />
-                          </div>
-                        </td>
-                        <td>
-                          <div className="tax">
-                            <input
-                              type="number"
-                              id="tax"
-                              className="form-control"
-                              placeholder="Tax"
-                              value={tax}
-                              onChange={(e) => setTax(e.target.value)}
-                            />
-                          </div>
-                        </td>
-                        <td>
-                          <div className="amount">{totalAmount.toFixed(2)}</div>
-                        </td>
-                        <hr />
-                      </>
-                    )}
                   </tr>
                 </tbody>
               </table>
               <div className="row">
                 <div className="col-8"></div>
                 <div className="col-4">
-                  <div>Excluding Tax : {withouttax}</div>
-                  <div>Total : {totalAmount.toFixed(2)}</div>
+                  <div>Subtotal: {subtotal.toFixed(2)}</div>
+                  <div>Total: {total.toFixed(2)}</div>
                 </div>
               </div>
             </div>
